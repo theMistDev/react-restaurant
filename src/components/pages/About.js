4c1f4c95fff082b1e792281a449cf32e1b1baf9c
themistdev <themistdev@gmail.com>
2022-04-27 10:55:04 +0100

    contact page updates

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../Button';
 import PageTitle from '../PageTitle';
 
 const About = () => {
@@ -6,11 +7,13 @@ const About = () => {
     <main>
       <PageTitle title="About Us" />
 
-      <div className="md:grid grid-cols-2">
-        <div className="col-span-1">
-          <h3 className="text-2xl font-bold">A clean Kitchen does wonders.</h3>
+      <div className="md:grid grid-cols-2 max-h-screen">
+        <div className="col-span-1 px-6 py-3 flex flex-col justify-around h-[80vh]">
+          <h3 className="text-2xl font-bold py-3">
+            A clean Kitchen does wonders.
+          </h3>
 
-          <p>
+          <p className="py-3 text-xl">
             If you’re interested in playing around with React, you can use an
             online code playground. Try a Hello World template on CodePen,
             CodeSandbox, or Stackblitz. If you prefer to use your own text
@@ -20,21 +23,20 @@ const About = () => {
             demos.
           </p>
 
-          <p>
+          <p className="py-3 text-xl">
             When starting a React project, a simple HTML page with script tags
             might still be the best option. It only takes a minute to set up! As
             your application grows, you might want to consider a more integrated
-            setup. There are several JavaScript toolchains we recommend for
-            larger applications. Each of them can work with little to no
-            configuration and lets you take full advantage of the rich React
-            ecosystem. Learn how.
+            setup..
           </p>
+
+          <Button title="Learn More" />
         </div>
 
-        <div className="col-span-1">
+        <div className="col-span-1 flex w-full">
           <img
-            className=" object-cover w-full"
-            src="https://images.unsplash.com/photo-1551218808-94e220e084d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmVzdGF1cmFudCUyMGtpdGNoZW58ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"
+            className="object-cover w-full"
+            src="https://images.unsplash.com/photo-1557955776-857434f1c951?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHJlc3RhdXJhbnQlMjBraXRjaGVufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
             alt="product"
           />
         </div>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,11 +1,71 @@
 import React from 'react';
 import PageTitle from '../PageTitle';
+import Button from '../Button';
 
 
 const Contact = () => {
   return (
     <main>
       <PageTitle title="Contact Us" />
+      <div className="md:grid grid-cols-2">
+        <div className="col-span-1 flex flex-col justify-center pl-5">
+          <div className="text-xl">
+            <h4 className="text-2xl">You can visit our Restaurant at:</h4>
+            <p className="pt-4">The Presidency Kitchen, </p>
+            <p>Aso Rock Villa, </p>
+            <p>Aso Rock, </p>
+            <p>Abuja, Nigeria</p>
+          </div>
+
+          <div className="py-5">
+            <div className="flex flex-row items-center">
+              <label>Phone Number:</label>
+              <p className="pl-3">+2347033244444</p>
+            </div>
+
+            <div className="flex flex-row items-center ">
+              <label>Email:</label>
+              <p className="pl-3">mail@coolbitefoods.com</p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <form className="w-full px-3 py-3">
+            <h4 className="font-bold text-2xl pb-3">Contact Us Form</h4>
+            <input
+              name="email"
+              type="email"
+              placeholder="Enter Your Email"
+              className="border-2 border-slate-400 w-full py-2 pl-2 placeholder-slate-500 rounded-lg"
+            />
+
+            <input
+              name="name"
+              type="text"
+              placeholder="Enter Your Name"
+              className="mt-2 border-2 border-slate-400 w-full py-2 pl-2 placeholder-slate-500 rounded-lg"
+            />
+
+            <input
+              name="phone"
+              type="tel"
+              placeholder="Enter Your Phone"
+              className="mt-2 border-2 border-slate-400 w-full py-2 pl-2 placeholder-slate-500 rounded-lg"
+            />
+
+            <textarea
+              name="message"
+              type="text"
+              placeholder="Enter Message Here"
+              className="mt-2 border-2 border-slate-400 w-full py-2 pl-2 placeholder-slate-500 rounded-lg"
+            rows='5'
+            />
+
+            <Button title="Send Message" />
+          </form>
+        </div>
+      </div>
     </main>
   );
 };
